83d57d932f21a2fd0c24e2549faff9e0b4028ba9
Nadia Bercha <nadiabercha@gmail.com>
2024-10-31 16:59:45 +0200

    interview modal

--- src/app/modules/freelanceinterview/pages/interviews/interview-feedback/interview-feedback.component.ts
+++ src/app/modules/freelanceinterview/pages/interviews/interview-feedback/interview-feedback.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { Freelanceinterview } from '../../../services/freelanceinterview.service';
+import { Freelanceinterview, Freelanceinterviewfeedback, FreelanceinterviewService } from '../../../services/freelanceinterview.service';
+import { FormService } from 'src/app/core/modules/form/form.service';
+import { UserService } from 'src/app/modules/user/services/user.service';
 
 @Component({
   selector: 'app-interview-feedback',
@@ -9,4 +11,73 @@ import { Freelanceinterview } from '../../../services/freelanceinterview.service
 export class InterviewFeedbackComponent {
 interview: Freelanceinterview;
 
+interviewIndex = 0;
+
+get interviews(): Record<string, unknown>[] {
+  return this.interview.feedback as unknown as Record<string, unknown>[];
+}
+
+form = this._form.getForm('interviewFeedback', {
+  formId: 'interviewFeedback',
+  title: 'Feedback',
+  components: [
+    {
+      name: 'Select',
+      key: 'author',
+      fields: [
+        {
+          name: 'Placeholder',
+          value: 'fill feedbacks author',
+        },
+        {
+          name: 'Label',
+          value: 'Author',
+        },
+        {
+          name: 'Items',
+          value: this._us.users,
+        }
+      ],
+    },
+    {
+      name: 'Boolean',
+      key: 'isApproved',
+      fields: [
+        {
+          name: 'Label',
+          value: 'Is Approved',
+        },
+      ],
+    },
+    {
+      name: 'Number',
+      key: 'grade',
+      focused: true,
+      fields: [
+        {
+          name: 'Placeholder',
+          value: 'fill feedbacks grade',
+        },
+        {
+          name: 'Label',
+          value: 'Grade',
+        },
+      ],
+    },
+  ],
+});
+
+addInterview(): void {
+  this.interview.feedback.push({} as Freelanceinterviewfeedback)
+}
+
+update() {
+  this._fis.update(this.interview);
+}
+
+constructor(
+  private _fis: FreelanceinterviewService,
+  private _form: FormService,
+  private _us: UserService
+) { }
 }
