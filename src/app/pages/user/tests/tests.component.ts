5fa0e0cf3c62e6e7c3f8eab3fe293ec47cfbf3c4
Nadia Bercha <nadiabercha@gmail.com>
2024-11-13 23:39:46 +0200

    test

--- src/app/pages/user/tests/tests.component.ts
+++ src/app/pages/user/tests/tests.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
-import { FreelancetestService } from 'src/app/modules/freelancetest/services/freelancetest.service';
+import { Freelancetest, FreelancetestService } from 'src/app/modules/freelancetest/services/freelancetest.service';
 @Component({
 	templateUrl: './tests.component.html',
 	styleUrls: ['./tests.component.scss']
 })
 export class TestsComponent {
+	testId: Freelancetest[] = [];
+
+	load(): void {
+		this._fts.get({}, { name: 'public' }).subscribe((testId) => {
+			console.log(testId) // Виводимо стартапи в консоль для перевірки
+			this.testId = testId; // Зберігаємо отримані дані в змінній `startups`
+		});
+	}
+
+	ngOnInit(): void {
+		this.load(); // Викликаємо функцію `load` під час ініціалізації компонента
+	}
 	constructor(
-		public fts: FreelancetestService
+		public fts: FreelancetestService,
+		private _fts: FreelancetestService
 	) { }
 }
