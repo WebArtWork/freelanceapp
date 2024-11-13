e372657805439183237337ba7ff7cb49728f026a
Nadia Bercha <nadiabercha@gmail.com>
2024-11-13 22:32:04 +0200

    curses

--- src/app/pages/user/courses/courses.component.ts
+++ src/app/pages/user/courses/courses.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
-import { FreelancecourseService } from 'src/app/modules/freelancecourse/services/freelancecourse.service';
+import { Freelancecourse, FreelancecourseService } from 'src/app/modules/freelancecourse/services/freelancecourse.service';
 @Component({
 	templateUrl: './courses.component.html',
 	styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent {
+	courseId: Freelancecourse[] = [];
+
+	load(): void {
+		this._fcs.get({}, { name: 'public' }).subscribe((courseId) => {
+			console.log(courseId) // Виводимо стартапи в консоль для перевірки
+			this.courseId = courseId; // Зберігаємо отримані дані в змінній `startups`
+		});
+	}
+
+	ngOnInit(): void {
+		this.load(); // Викликаємо функцію `load` під час ініціалізації компонента
+	}
+
 	constructor(
-		public fcs: FreelancecourseService
+		public fcs: FreelancecourseService,
+		private _fcs: FreelancecourseService
 	) { }
 }
